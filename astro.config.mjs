eaf4c5aebd2b9aed763b950af794a2042cc8660b
Samuel Briole <samuel.briole@gmail.com>
2023-08-26 23:56:07 +0200

    deploy website with github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://samuelbriole.com",
   integrations: [react(), tailwind({ applyBaseStyles: false })],
 });
